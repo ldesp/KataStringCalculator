1ef65baec2b2fe2b19c8e242f715ad2f3a9fd421
Franz Fallavier <ffallavier@gmail.com>
2016-03-10 15:47:47 +0100

    Commit For End Change

--- src/test/java/com/hb/formation/KataStringCalculator/MethodAddTest.java
+++ src/test/java/com/hb/formation/KataStringCalculator/MethodAddTest.java
@@ -2,46 +2,31 @@ package com.hb.formation.KataStringCalculator;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
 import org.junit.Test;
-
 
 public class MethodAddTest {
 
 	@Test
 	public void AddShouldReturnZeroWithEmptyString() {
 		StringCalculator calculator = new StringCalculator();
-		assertEquals(0, calculator.add(""));
+		assertEquals( 0, calculator.add(""));
 	}
 
 	@Test
 	public void AddShouldReturnOneWithValueOne() {
 		StringCalculator calculator = new StringCalculator();
-		assertEquals(1, calculator.add("1"));
+		assertEquals( 1, calculator.add("1"));
 	}
-
+	
 	@Test
 	public void AddShouldReturnTwoWithValueTwo() {
 		StringCalculator calculator = new StringCalculator();
-		assertEquals(2, calculator.add("2"));
+		assertEquals( 2, calculator.add("2"));
 	}
-
+	
 	@Test
 	public void AddShouldReturnThreeWithTwoNumbers() {
 		StringCalculator calculator = new StringCalculator();
-		assertEquals(3, calculator.add("1,2"));
-	}
-
-	@Test
-	public void givenUnknownNumbers_whenAddNumbers_thenSum() {
-		// Given
-		StringCalculator sc = new StringCalculator();
-		String numbers = "1,2,3,4";
-
-		// When
-		int result = sc.add(numbers);
-
-		// Then
-		Assert.assertEquals(result, 10);
+		assertEquals( 3, calculator.add("1,2"));
 	}
 }
